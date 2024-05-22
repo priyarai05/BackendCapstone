af0f3ca1774c0c195afcad892912d4a22aebe941
priyarai05 <pri2605@gmail.com>
2024-05-22 11:33:43 +0530

    class 1 router, logs and error

---
+++ server/index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = process.env.PORT || 3000;
+const fs = require("fs");
+const path = require("path");
+const logStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
+  flags: "a",
+});
+const errorStream = fs.createWriteStream(path.join(__dirname, "error.txt"), {
+  flags: "a",
+});
+
+const authRoutes = require("./routes/auth");
+const jobRoutes = require("./routes/job");
+const { error } = require("console");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  const now = new Date();
+  const time = `${now.toLocaleTimeString()}`;
+  const log = `${req.method} ${req.originalUrl} ${time}`;
+  logStream.write(log + "\n");
+  console.log(req.originalUrl, time);
+  next();
+});
+
+app.use("/api/auth", authRoutes);
+app.use("/api/job", jobRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello World").status(200);
+});
+
+// app.use((err, req, res, next) => {
+//   const now = new Date();
+//   const time = `${now.toLocaleTimeString()}`;
+//   const log = `${req.method} ${req.originalUrl} ${time} ${err.stack}`;
+//   errorStream.write(error + "\n");
+//   res.status(500).send("Internal server error");
+// });
+
+app.use((req, res, next) => {
+  const now = new Date();
+  const time = `${now.toLocaleTimeString()}`;
+  const error = `${req.method} ${req.originalUrl} ${time}`;
+  errorStream.write(error + "\n");
+  res.status(404).send("Route not found");
+});
+
+app.listen(port, () => {
+  console.log(`server running on http://localhost:${port}`);
+});
